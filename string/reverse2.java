49bf96d6a4b2d10b9f10cd50247bd17655d64558
MDABUTALHA777 <74580937+MDABUTALHA777@users.noreply.github.com>
2022-08-22 11:49:20 +0530

    Add files via upload

---
+++ string/reverse2.java
@@ -0,0 +1,33 @@
+package first;
+
+
+public class reverse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// iNeuron ---> norueNi
+		
+		String s1="Think Twice";
+		String s2="";
+		String s3="";
+		
+		
+		for(int i=s1.length()-1; i>=0; i--)
+		{
+			s2=s2+s1.charAt(i);
+		}
+	
+		//System.out.println(s2);
+
+		
+		String a[]=s2.split(" ");
+	
+		for(int j=a.length-1;j>=0;j--)
+		{
+			s3=s3+a[j]+ " ";
+	    }
+		System.out.println(s3);
+	   
+}
+}
